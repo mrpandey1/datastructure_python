371da15ef8e03b9ebfeda982d976bedce6b4bae7
mrpandey1 <pandeynishant12345@gmail.com>
2020-05-29 14:08:44 +0530

    insertion in linked list

---
+++ LinkedList/SinglyLinkedList/insertion.py
@@ -0,0 +1,44 @@
+class Node:
+    def __init__(self,data):
+        self.data=data
+        self.next=None
+class LinkedList:
+    def __init__(self):
+        self.head=None
+    def printList(self): 
+        temp = self.head 
+        while (temp): 
+            print(temp.data,end=' ')
+            temp = temp.next
+        print()
+    def InsertAtEnd(self,data):
+        new_node=Node(data)
+        if self.head is None:
+            self.head=new_node
+            return
+        temp=self.head
+        while temp.next:
+            temp=temp.next
+        temp.next=new_node
+    def InsertAtStart(self,data):
+        new_node=Node(data)
+        new_node.next=self.head
+        self.head=new_node
+    def InsertAfter(self,prev,data):
+        temp=self.head
+        while temp.next and temp.data!=prev:
+            temp=temp.next
+        if temp.data!=prev:
+            print("Not Found")
+            return
+        new_node=Node(data)
+        new_node.next=temp.next
+        temp.next=new_node
+llist = LinkedList()
+llist.InsertAtEnd(6)
+llist.printList()
+for i in range(4):
+    llist.InsertAtStart(i)
+llist.printList()
+llist.InsertAfter(0,5)
+llist.printList()
