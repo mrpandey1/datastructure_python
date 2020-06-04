f1da250893de0e1eba1cc3f02e9801402040841d
mrpandey1 <pandeynishant12345@gmail.com>
2020-06-04 14:08:41 +0530

    delete element from start

---
+++ LinkedList/DoublyLinkedList/deletion.py
@@ -0,0 +1,34 @@
+class Node:
+    def __init__(self,data):
+        self.data=data
+        self.next=None
+        self.prev=None
+class DoublyLinkedList:
+    def __init__(self):
+        self.head=None
+    def addatstart(self,data):
+        new=Node(data)
+        new.next=self.head
+        new.prev=None
+        if self.head is not None:
+            self.head.prev=new
+        self.head=new
+    def deletestart(self):
+        temp=self.head
+        self.head=self.head.next
+        self.head.prev=None
+    def printList(self):
+        temp=self.head
+        while temp:
+            print(temp.data,end=' ')
+            temp=temp.next
+        print()
+llist=DoublyLinkedList()
+llist.addatstart(1)
+llist.addatstart(2)
+llist.addatstart(3)
+llist.addatstart(4)
+llist.addatstart(5)
+llist.printList()
+llist.deletestart(
+llist.printList()
