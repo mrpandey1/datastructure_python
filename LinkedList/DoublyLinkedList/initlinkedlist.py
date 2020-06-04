99d6d957b8a344afaf333b7fb7ff9ef1ea73439d
mrpandey1 <pandeynishant12345@gmail.com>
2020-06-04 12:35:45 +0530

    add at start

--- LinkedList/DoublyLinkedList/initlinkedlist.py
+++ LinkedList/DoublyLinkedList/initlinkedlist.py
@@ -13,17 +13,6 @@ class DoublyLinkedList:
         if self.head is not None:
             self.head.prev=new
         self.head=new
-    def addatend(self,data):
-        new_node=Node(data)
-        if self.head is None:
-            self.head=new_node
-            return
-        temp=self.head
-        while temp.next:
-            temp=temp.next
-        temp.next=new_node
-        new_node.prev=temp
-        
     def printList(self):
         temp=self.head
         while temp:
@@ -36,9 +25,4 @@ llist.addatstart(2)
 llist.addatstart(3)
 llist.addatstart(4)
 llist.addatstart(5)
-llist.addatend(5)
-llist.addatend(4)
-llist.addatend(3)
-llist.addatend(2)
-llist.addatend(1)
 llist.printList()
