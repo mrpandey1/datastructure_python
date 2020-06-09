996eccf482ea44fab568fe3faf6d02f979f9eb48
mrpandey1 <pandeynishant12345@gmail.com>
2020-06-09 13:05:14 +0530

    shell sort

---
+++ Sorting list/Shell sort.py
@@ -0,0 +1,16 @@
+def shellSort(a):
+    n=len(a)
+    gap=n//2
+    while gap>0:
+        for i in range(gap,n):
+            temp=a[i]
+            j=i
+            while j>=gap and a[j-gap]>temp:
+                a[j]=a[j-gap]
+                j-=gap
+            a[j]=temp
+        gap//=2
+a=[12,34,54,2,3]
+n=len(a)
+shellSort(a)
+print(a)
