f11c96589a89f998f45322b3561c55b0eb50ca7a
mrpandey1 <pandeynishant12345@gmail.com>
2020-06-14 14:06:24 +0530

    fullTree

---
+++ Tree/Full binary tree.py
@@ -0,0 +1,26 @@
+class Node:
+    def __init__(self,item):
+        self.item=item
+        self.leftChild=None
+        self.rightChild=None
+def isFullTree(root):
+    if root is None:
+        return True
+    if root.leftChild is  None and root.rightChild is None:
+        return True
+    if root.leftChild is not None and root.rightChild is not None:
+        return (isFullTree(root.leftChild) and isFullTree(root.rightChild))
+    return False
+root = Node(1)
+root.rightChild = Node(3)
+root.leftChild = Node(2)
+root.leftChild.leftChild = Node(4)
+root.leftChild.rightChild = Node(5)
+root.rightChild.leftChild = Node(6)
+root.rightChild.rightChild = Node(7)
+root.rightChild.leftChild.leftChild = Node(8)
+
+if isFullTree(root):
+    print("The tree is a full binary tree")
+else:
+    print("The tree is not a full binary full")
